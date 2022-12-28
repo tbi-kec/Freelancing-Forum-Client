7f7f513eea73f26f2facc72be5d0bc92a9cb0b21
JANARTHANAN-T <janathangavel0911@gmail.com>
2022-12-28 22:41:20 +0530

    notification

--- src/components/homepage/Notification.jsx
+++ src/components/homepage/Notification.jsx
@@ -8,7 +8,8 @@ export default function Notification({notification}) {
     <div className='notification-container my-2'>
         <div className="row p-2 ">
             <div className="col-8">
-                <h6 className='mt-1 text-light'>{notification.p_id.title}</h6>
+                <h6 className='mt-1 text-light fw-bold'>{notification.p_id.title}</h6>
+                <p className='notification-msg text-start'>{notification.message}</p>
             </div>
             <div className="col">
                 {notification.notify_type==1?
@@ -16,8 +17,8 @@ export default function Notification({notification}) {
                     <button className='btn success px-2 py-1 me-2' >Accept</button>
                     <button className='btn danger px-2 py-0 '>Reject</button>
                 </div>
-                :<div className='d-flex justify-content-end'>
-                <h6>{notification.message}</h6>
+                :<div className='d-flex justify-content-end text-danger'>
+                <i class="fa-sharp fa-solid fa-xmark"></i>
             </div>
 }
 
