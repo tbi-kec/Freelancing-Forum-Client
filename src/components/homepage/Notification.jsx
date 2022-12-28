88c46291dd6f101b60f681a36f74c7c039188d7e
LOGESH-B <logeshbommannan@gmail.com>
2022-12-28 21:46:53 +0530

    notify_condition

--- src/components/homepage/Notification.jsx
+++ src/components/homepage/Notification.jsx
@@ -8,15 +8,17 @@ export default function Notification({notification}) {
     <div className='notification-container my-2'>
         <div className="row p-2 ">
             <div className="col-8">
-                <h6 className='mt-1 text-light'>{notification.message}</h6>
+                <h6 className='mt-1 text-light'>{notification.p_id.title}</h6>
             </div>
             <div className="col">
-                {notification.message=='pending-user'?
+                {notification.notify_type==1?
                 <div className='d-flex justify-content-end'>
                     <button className='btn success px-2 py-1 me-2' >Accept</button>
                     <button className='btn danger px-2 py-0 '>Reject</button>
                 </div>
-                :""
+                :<div className='d-flex justify-content-end'>
+                <h6>{notification.message}</h6>
+            </div>
 }
 
             </div>
