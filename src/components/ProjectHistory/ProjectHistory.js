4a0d4410baa349692d2996204b6b017e7a696e34
JANARTHANAN-T <janathangavel0911@gmail.com>
2022-12-29 20:04:37 +0530

    project histroy map

--- src/components/ProjectHistory/ProjectHistory.js
+++ src/components/ProjectHistory/ProjectHistory.js
@@ -6,16 +6,19 @@ export default function ProjectHistory({project}) {
 
     <div>
     {project.length!=0 ?
+    <div>
         <div className="student-card project-history">
             <div className="title">
                 
-                <h2>P</h2>
+                <h2>Project - History</h2>
             </div>
+            {project.map((e,i)=>{
+            return(
             <div className="card-group">
                 {project.length&&
                 <div className="inner-card">
                     <div className="inner-card-head">
-                        <h2>Driver Drowsiness Project</h2>
+                        <h2>{project.title}</h2>
                     </div>
                     <div className="domain">
                        <span>Aritificial Inetlligence</span>
@@ -28,7 +31,11 @@ export default function ProjectHistory({project}) {
                 </div>
 }
             </div>
-        </div> :
+            )
+        })}
+        </div>
+      </div>
+         :
         <></>
     
     }
