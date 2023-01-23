acc5a21f91446a5dd9e47115a0ab0319c72356fe
Sujit Balasubramanian <sujitbalasubramanianr@gmail.com>
2023-01-23 10:22:00 +0530

    Home and Footer

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react'
 import AllRoutes from './AllRoutes'
 import {BrowserRouter as Router} from 'react-router-dom'
 import Alert from './components/Alert/Alert'
 import './App.css'
-import { setCurrentUser } from './actions/currentUser'
-import { useDispatch } from 'react-redux'
-import { getAllProjects } from './actions/project'
-import { getAllUsers } from './actions/user'
-import { getConstants } from './actions/constant'
-import { getMyDetails } from './actions/myDetails'
-import { getRequestedProjects } from './actions/admin'
+import {setCurrentUser} from './actions/currentUser'
+import {useDispatch} from 'react-redux'
+import {getAllProjects} from './actions/project'
+import {getAllUsers} from './actions/user'
+import {getConstants} from './actions/constant'
+import {getMyDetails} from './actions/myDetails'
+import {getRequestedProjects} from './actions/admin'
 const App = () => {
   const dispatch = useDispatch();
-  const getData = async()=>{
+  const getData = async () => {
     const token = await localStorage.getItem("freelance");
-    if(token){
+    if (token) {
       dispatch(setCurrentUser(JSON.parse(token)))
     }
   }
-  useEffect(()=>{
+  useEffect(() => {
     getData();
     dispatch(getAllProjects())
     dispatch(getAllUsers())
     dispatch(getConstants())
     dispatch(getMyDetails())
     dispatch(getRequestedProjects())
-  },[dispatch])
+  }, [dispatch])
   return (
     <div className='app-container'>
       <Router>
-          <Alert />
-          <AllRoutes />
+        <Alert />
+        <AllRoutes />
       </Router>
     </div>
   )
