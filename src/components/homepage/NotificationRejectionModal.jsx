db9b61441bc380fcf1eda45edd16339f0d1e1773
LOGESH-B <logeshbommannan@gmail.com>
2023-01-28 06:10:40 +0530

    Notification/reject

---
+++ src/components/homepage/NotificationRejectionModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function NotificationRejectionModal(props) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [message,setMessage]=useState("")
+
+    const handleRejectUser = (e)=>{
+        e.preventDefault();
+        // dispatch(setAlert("Rejecting freelancer","info"))
+        // dispatch(acceptOrRejectUser({u_id:id,status:"rejected",message},navigate))
+    
+      }
+  return (
+    <div>
+      {/* model */}
+      <div className="modal fade " id={`rejection-${props.id}`} tabIndex="-1" role='dialog' aria-labelledby="exampleModalLabel" >
+        <div className="modal-dialog modal-dialog-centered modal-lg">
+          <div className="modal-content p-5">
+            <label className="form-label">Reason for rejection</label>
+            <textarea name="" value={message} className="form-control my-3" onChange={e=>setMessage(e.target.value)}></textarea>
+            <button onClick={e=>handleRejectUser(e,props.id)} data-bs-dismiss="modal" aria-label="Close" className="btn btn-danger">Reject</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NotificationRejectionModal
