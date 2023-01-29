f5a3f3b7af481a1c6c86934f19cede79a23e610a
LOGESH-B <logeshbommannan@gmail.com>
2023-01-29 12:10:40 +0530

    progress/change

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch,useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { updateStatus } from '../../actions/project';
 import starColor from '../../assets/Color-star.png'
 import starDull from '../../assets/dull-star.png'
@@ -9,76 +9,75 @@ import moment from 'moment';
 import './ProgressBar.css'
 import { developerUpdateRating } from '../../actions/myDetails'
 import { updateStatusToVerify } from '../../actions/project';
-function ProgressBar({status,p_id,c_id,d_id,deadline,pay}) {
-   const navigate = useNavigate();
-   const dispatch = useDispatch();  
-   const [userRating,setUserRating]=useState(0)
-   const [driveLink,setDriveLink]=useState()
-   const [amount,setAmount]=useState(pay)
-  
+function ProgressBar({ status, p_id, c_id, d_id, deadline, pay }) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [userRating, setUserRating] = useState(0)
+    const [driveLink, setDriveLink] = useState()
+    const [amount, setAmount] = useState(pay)
+
     const [progressStepsNum, setprogressStepsNum] = useState(0);
-  const user = useSelector((state)=>(state.myDetailsReducer))
- 
+    const user = useSelector((state) => (state.myDetailsReducer))
+
     const nextprogress = () => {
-        setprogressStepsNum(prevprogressStep=>prevprogressStep+1);
-                    dispatch(updateStatus({p_id,status:progressStepsNum+1},navigate))  
+        setprogressStepsNum(prevprogressStep => prevprogressStep + 1);
+        dispatch(updateStatus({ p_id, status: progressStepsNum + 1 }, navigate))
     }
     const date = new Date()
- 
-   
+
+
 
     useEffect(() => {
-        const setinital=async ()=>{
-            
-            if (status === "assigned") { 
+        const setinital = async () => {
+
+            if (status === "assigned") {
                 setprogressStepsNum(0);
             }
             else if (status === "partial") {
-                setprogressStepsNum(1) }
-            else if(status==="testing")
+                setprogressStepsNum(1)
+            }
+            else if (status === "testing")
                 setprogressStepsNum(2)
-            else if(status==="verify"){ 
-                setprogressStepsNum(3); 
-            }else{
-                setprogressStepsNum(4);
+            else {
+                setprogressStepsNum(3);
             }
         }
- 
+
         setinital();
     }, [status])
 
-    useEffect(()=>{
-        setTimeout(()=>{
-               const updateProgressbar = () => {
-    
-            const progressSteps = document.querySelectorAll(".progress-step");
-            const progress = document.getElementById("progress");
-            progressSteps.forEach((progressStep, idx) => {
-                return idx < progressStepsNum + 1 ?
-                    progressStep.classList.add("progress-step-active")
-                    :
-                    progressStep.classList.remove("progress-step-active");
-            });
-            const progressActive = document.querySelectorAll(".progress-step-active");
-            progress.style.width = ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
-        
-    }
-                updateProgressbar()
+    useEffect(() => {
+        setTimeout(() => {
+            const updateProgressbar = () => {
 
-    },1000)
-    },[progressStepsNum]) 
+                const progressSteps = document.querySelectorAll(".progress-step");
+                const progress = document.getElementById("progress");
+                progressSteps.forEach((progressStep, idx) => {
+                    return idx < progressStepsNum + 1 ?
+                        progressStep.classList.add("progress-step-active")
+                        :
+                        progressStep.classList.remove("progress-step-active");
+                });
+                const progressActive = document.querySelectorAll(".progress-step-active");
+                progress.style.width = ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
 
-    const handleRating = (e) =>{
-            e.preventDefault();
-         nextprogress();
-         dispatch(updateStatusToVerify({drive_link:driveLink,amount,p_id},navigate))
-            setTimeout(() => {
-                dispatch(developerUpdateRating({rating:parseInt(userRating),u_id:d_id,p_id}))
-            }, 2000);
+            }
+            updateProgressbar()
+
+        }, 1000)
+    }, [progressStepsNum])
+
+    const handleRating = (e) => {
+        e.preventDefault();
+        nextprogress();
+        dispatch(updateStatusToVerify({ drive_link: driveLink, amount, p_id }, navigate))
+        setTimeout(() => {
+            dispatch(developerUpdateRating({ rating: parseInt(userRating), u_id: d_id, p_id }))
+        }, 2000);
     }
 
     return (
-    <div>
+        <div>
             {/* modal - next progress */}
             <div
                 className="modal fade "
@@ -130,42 +129,42 @@ function ProgressBar({status,p_id,c_id,d_id,deadline,pay}) {
                     <div className="modal-content text-center">
 
                         <div>
-                        <div className="row d-flex align-items-end">
-                            <div className="col-sm-6">
-                            <div className="fs-6 m-5">
-                                Please provide the recorded video of your project prototype and share via drive link with public accessiblity.
+                            <div className="row d-flex align-items-end">
+                                <div className="col-sm-6">
+                                    <div className="fs-6 m-5">
+                                        Please provide the recorded video of your project prototype and share via drive link with public accessiblity.
+                                    </div>
+                                    <div className='mx-5 mb-4'>
+                                        <input type="text" className='form-control' placeholder='Drive Link of your project prototype' value={driveLink} onChange={(e) => setDriveLink(e.target.value)} style={{ backgroundColor: '#EFEFEF' }} />
+                                    </div>
+                                </div>
+                                <div className="col-sm-6">
+                                    <div className="fs-5 m-5">
+                                        Confirm the amount you have paid for this project.
+                                    </div>
+                                    <div className='mx-5 mb-4'>
+                                        <input type="text" className='form-control' placeholder='Project payment' style={{ backgroundColor: '#EFEFEF' }} value={amount} onChange={(e) => setAmount(e.target.value)} />
+                                    </div>
+                                </div>
                             </div>
-                            <div className='mx-5 mb-4'>
-                            <input type="text" className='form-control' placeholder='Drive Link of your project prototype' value={driveLink} onChange={(e)=>setDriveLink(e.target.value)} style={{backgroundColor:'#EFEFEF'}} />
-                            </div>
-                            </div>
-                            <div className="col-sm-6">
-                            <div className="fs-5 m-5">
-                                Confirm the amount you have paid for this project.
-                            </div>
-                            <div className='mx-5 mb-4'>
-                            <input type="text" className='form-control' placeholder='Project payment' style={{backgroundColor:'#EFEFEF'}} value={amount} onChange={(e)=>setAmount(e.target.value)}  />
-                            </div>
-                            </div>
-                        </div>
                             <div className="fs-4 m-5">
                                 Please Confirm that your Project is completed and rate the work done.
                                 <div>    {(
-      function () {
-        var rate=[]
-        for(var i=0;i<userRating;i++){
-          rate.push(<img key={i} src={starColor} id={i+1} alt='star' height='30px' onClick={(e)=>setUserRating(e.target.id)} />)
-        }
-        for(let j=0;j<5-userRating;j++){
-          rate.push(<img key={j} src={starDull} alt='star' id={i+j+1} height='25px' onClick={(e)=>setUserRating(e.target.id)} />)
-        }
-        return(
-          <div key={rate.length}>
-          {rate}
-          </div>
-        )
-      }
-    ())}</div>
+                                    function () {
+                                        var rate = []
+                                        for (var i = 0; i < userRating; i++) {
+                                            rate.push(<img key={i} src={starColor} id={i + 1} alt='star' height='30px' onClick={(e) => setUserRating(e.target.id)} />)
+                                        }
+                                        for (let j = 0; j < 5 - userRating; j++) {
+                                            rate.push(<img key={j} src={starDull} alt='star' id={i + j + 1} height='25px' onClick={(e) => setUserRating(e.target.id)} />)
+                                        }
+                                        return (
+                                            <div key={rate.length}>
+                                                {rate}
+                                            </div>
+                                        )
+                                    }
+                                        ())}</div>
                             </div>
                             <div className="d-flex justify-content-around">
                                 <button
@@ -190,7 +189,7 @@ function ProgressBar({status,p_id,c_id,d_id,deadline,pay}) {
             </div>
 
             {/* progress container */}
-           <div className='container text-center'>
+            <div className='container text-center'>
                 <div className="progressbar">
                     <div className="progress" id="progress"></div>
                     <div
@@ -201,30 +200,30 @@ function ProgressBar({status,p_id,c_id,d_id,deadline,pay}) {
                     <div className="progress-step" data-title="Completed"><i className="fas fa-check-circle"></i></div>
                 </div>
                 <div className='row'>
-                <div className="col-sm-6">
-                <div className="text-start py-3" >
-                  <b>Deadline:</b> <span className={date > deadline ? 'text-danger':'text-primary'} >{moment(deadline).fromNow()}</span> 
-                </div>
-                </div>
-                <div className="col-sm-6 text-end">
-                {progressStepsNum <2 && d_id===user?.data?._id ?
-                <div className="progress-btn">
-                    <div href="#" className="btn" data-bs-toggle="modal"
-                        data-bs-target="#toggle_model_request">Next</div>
-                </div>
-                :""}
-                {progressStepsNum >=2 && c_id===user?.data?._id   ?
-                <div className="progress-btn">
-                    <div href="#" className="btn" data-bs-toggle="modal"
-                        data-bs-target="#toggle_model_rate">Confirm and rate</div>
-                </div>
-                :""}
-                </div>
+                    <div className="col-sm-6">
+                        <div className="text-start py-3" >
+                            <b>Deadline:</b> <span className={date > deadline ? 'text-danger' : 'text-primary'} >{moment(deadline).fromNow()}</span>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 text-end">
+                        {progressStepsNum < 2 && d_id === user?.data?._id ?
+                            <div className="progress-btn">
+                                <div href="#" className="btn" data-bs-toggle="modal"
+                                    data-bs-target="#toggle_model_request">Next</div>
+                            </div>
+                            : ""}
+                        {progressStepsNum >= 2 && c_id === user?.data?._id ?
+                            <div className="progress-btn">
+                                <div href="#" className="btn" data-bs-toggle="modal"
+                                    data-bs-target="#toggle_model_rate">Confirm and rate</div>
+                            </div>
+                            : ""}
+                    </div>
 
                 </div>
-                
+
             </div>
-    
+
         </div>
 
     )
